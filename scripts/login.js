9204c398c3f192b4d2229a4b41183f9e65abdcff
seaiceeyes <seeiceeyes@gmail.com>
2025-07-13 01:46:18 +0300

    add loading welcome page to login

--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,31 @@
 const usernameInput = document.getElementById("username");
 
+const textClass = document.querySelector('.login-welcome-text');
+const containerClass = document.querySelector('.login-form');
+const welcomeClass = document.querySelector('.login-welcome');
+
+
+welcomePage = () => {
+        containerClass.style.visibility = 'hidden';
+        welcomeClass.style.visibility = 'visible';
+};
+
 username.addEventListener('keypress', (e) => {
     if (e.key === 'Enter'){
         const username = usernameInput.value.trim();
 
         localStorage.setItem('username', username);
-        window.location.href = 'index.html';
+        textClass.textContent = `${username}`;
+
+        welcomePage();
+
+        setTimeout(() => {
+            window.location.href = 'index.html'; 
+            containerClass.style.visibility = 'visible';
+            welcomeClass.style.visibility = 'hidden';
+        }, 3000);
+
     }
 });
+
 
