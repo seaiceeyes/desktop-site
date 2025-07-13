78b78c5d6e04d569da9f6e12e2a043838343548d
seaiceeyes <seeiceeyes@gmail.com>
2025-07-13 09:31:37 +0300

    implemented all functions of the start menu

---
+++ scripts/navigation-bar.js
@@ -0,0 +1,52 @@
+const buttonContainer = document.querySelector(".button-menu");
+
+const navBarButton = document.querySelector(".nav-bar-button");
+
+const sleepButton = document.querySelector("[data-sleep-button]");
+const sleepScreen =document.querySelector(".sleep-screen");
+
+const shutDownButton = document.querySelector("[data-shut-down-button]");
+const shutDown = document.querySelector(".shout-down");
+
+const restartButton = document.querySelector("[data-restart-button]");
+const restartScreen = document.querySelector(".restart-screen");
+
+
+navBarButton.addEventListener('click', () => {
+    buttonContainer.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+    if (!buttonContainer.contains(e.target) && e.target !== navBarButton) {
+        buttonContainer.classList.remove("active");
+    }
+});
+
+eventListener(shutDownButton, shutDown, 5000);
+eventListener(restartButton, restartScreen, 2000);
+
+
+function eventListener (buttonName, className, timeout) {
+    buttonName.addEventListener("click", () => {
+        className.classList.add("active");
+        buttonContainer.classList.remove("active");
+
+    setTimeout(() => {
+        window.location.href = 'login.html';
+        className.classList.remove("active");
+    }, timeout);
+});
+}
+
+sleepButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    sleepScreen.classList.add("active");
+    buttonContainer.classList.remove("active");
+});
+
+document.addEventListener("click", () => {
+    if(sleepScreen.classList.contains('active')) {
+        sleepScreen.classList.remove("active");
+    }
+});
+
